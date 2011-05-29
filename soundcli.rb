7ad16637e950ef9f57be2fdb727cc9e426ea0202
Ricardo <ricardo.wurmus@gmail.com>
2011-05-29 22:09:59 +0800

    merge run.rb and soundcli.rb

--- soundcli.rb
+++ soundcli.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require "#{File.dirname(__FILE__)}/settings"
 require "#{File.dirname(__FILE__)}/access_token"
 require "#{File.dirname(__FILE__)}/player"
@@ -144,4 +146,27 @@ EOF
 		})
 		puts res[:response]
 	end
+end
+
+#----------------------------------------------------
+# MAIN
+#----------------------------------------------------
+
+Settings::init(ARGV)
+app = SoundCLI.new
+
+if ARGV.length < 1
+	puts app.usage
+	Process.exit 1
+end
+
+if app.features.include? ARGV[0].to_sym
+	if ARGV[1]
+		app.method(ARGV[0]).call(ARGV[1..-1])
+	else
+		app.method(ARGV[0]).call
+	end
+else
+	$stderr.puts "No such action: #{ARGV[0]}"
+	app.usage and Process.exit(1)
 end
