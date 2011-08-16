00f1c31c81d124901efc9b08e93394a809946b19
Ricardo <ricardo.wurmus@gmail.com>
2011-08-16 22:05:37 +0800

    read from configuration file

--- lib/soundcli/settings.rb
+++ lib/soundcli/settings.rb
@@ -4,7 +4,7 @@ module Settings
   PRG_NAME      = "soundCLI"
   CLIENT_ID     = "a0f81a06bbce9c2995ef16b87eb87820"
   CLIENT_SECRET = "1871359cfbc4bb8da074de5005adbac8"
-  REDIRECT_URI  = "http://soundcli.wurmus.de/oauth-callback"
+  REDIRECT_URI  = "http://soundcli.elephly.net/oauth-callback"
   CONNECT_URI   = "https://soundcloud.com/connect?client_id=#{CLIENT_ID}&response_type=code&redirect_uri=#{REDIRECT_URI}"
   TOKEN_URI     = "https://api.soundcloud.com/oauth2/token"
   API_URI_SSL   = "https://api.soundcloud.com"
@@ -22,7 +22,7 @@ module Settings
     end
     $stderr.puts "No config file found or error parsing it. Ignoring." unless parse_config
 
-    return true if all['auth_type'] == 'login'
+    return true if all['auth-type'] == 'login'
 
     # check if an auth code exists
     unless all.has_key? 'code'
@@ -50,19 +50,33 @@ EOF
     config_path = config_path + "/#{PRG_NAME.downcase}"
     cf = "#{config_path}/#{config_file}"
     @config['path'] = config_path
-    puts config_path
 
-    # TODO: read from config instead
-    @config['auth_type'] = 'login'  # 'authentication_code'
-    @config['verbose'] = false
+    # defaults
+    @config['auth-type'] = 'login'
+    @config['verbose'] = 'normal'
     @config['buffer-size'] = 512_000
-    @config['comment_width'] = 50
-    @config['comment_indent_width'] = 4
+    @config['comment-width'] = 50
+    @config['comment-indent-width'] = 4
+
+    Helpers::sayn(config_path, :debug)
 
     return false unless File.exists? cf
 
+    # overwrite from config file
     begin
-      @config.merge!(JSON.parse(File.read(cf)))
+      json = File.read(cf)
+      # strip comments and empty lines
+      json.gsub!(/^ *#.*\n/,"")
+      json.gsub!(/^ *\n/,"")
+      # jsonify keys
+      json.gsub!(/^([^ :]*)/, "\"\\1\"")
+      # add commas at the end of lines and wrap in braces
+      json.gsub!(/\n/,",")
+      json = "{" << json
+      json[-1] = "}"
+      Helpers::sayn(json, :debug)
+
+      @config.merge!(JSON.parse(json))
       return true
     rescue
       $stderr.puts "Your configuration file contains errors."
